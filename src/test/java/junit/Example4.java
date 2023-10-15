79ecf173a3ba40e87085a18c54f0ac4676a75ed1
Hatef Palizgar <hatef.palizgar@gamesys.co.uk>
2023-10-15 11:23:55 +0100

    adds Example4

---
+++ src/test/java/junit/Example4.java
@@ -0,0 +1,42 @@
+package junit;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Example4 {
+
+    @BeforeAll
+    public static void setUpBeforeClass() {
+        System.out.println("Run BEFORE all tests");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Run BEFORE each test");
+    }
+
+    @AfterAll
+    public static void tearDownAfterClass () {
+        System.out.println("Run AFTER all tests");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Run AFTER each test");
+    }
+
+
+    @Test
+    public void first(){
+        System.out.println("Implementation of the first test");
+        assertTrue( 1 != 0); // PASS
+    }
+
+    @Test
+    public void second() {
+        System.out.println("Implementation of the second test");
+        assertTrue( 1 != 0); // PASS
+    }
+
+}
